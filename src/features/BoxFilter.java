341863cb01548fd37e5d7f7a2cebce240880f847
unknown <sinceramuna@gmail.com>
2017-12-28 01:00:42 +0600

    first commit

---
+++ src/features/BoxFilter.java
@@ -0,0 +1,58 @@
+package features;
+
+import ioRelatedClasses.ReadImage;
+import ioRelatedClasses.WriteImage;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class BoxFilter {
+	ReadImage readimage;
+	WriteImage writeImage;
+	//start constructor
+	public BoxFilter(File file)	throws IOException {
+		// TODO Auto-generated constructor stub
+
+		readimage=new ReadImage(file);
+		writeImage=new WriteImage(readimage);
+		Box();
+	
+	}//end constructor
+	
+	private void Box() throws IOException
+	{
+		
+		int filterHeight=5,filterWidth= 5;
+			
+		for(int y = 0; y < readimage.getHeight(); y++) 
+			    for(int x = 0; x < readimage.getWidth(); x++) 
+			    { 
+			        int r = 0, g = 0, b = 0; 
+			        List<Integer> Rlist= new ArrayList<Integer>();
+			        List<Integer> Glist= new ArrayList<Integer>();
+			        List<Integer> Blist= new ArrayList<Integer>();
+			        //multiply every value of the filter with corresponding image pixel 
+			        for(int filterY = 0; filterY < filterHeight; filterY++) 
+			        for(int filterX = 0; filterX < filterWidth ; filterX++) 
+			        { 
+			            int imageX = (x - filterWidth / 2 + filterX + readimage.getWidth()) % readimage.getWidth(); 
+			            int imageY = (y - filterHeight / 2 + filterY + readimage.getHeight()) % readimage.getHeight(); 
+			            
+			            r+= (int)readimage.getRed(imageY, imageX); 
+			            g+= (int)readimage.getGreen(imageY, imageX); 
+			            b+= (int)readimage.getBlue(imageY, imageX); 
+			        } 
+		
+			        //truncate values smaller than zero and larger than 255 
+			        b =(int)(b/9); 
+			        g =(int)(b/9);  
+			        r =(int)(b/9); 
+			     //   System.out.println(r+" "+g+" "+b);
+			        writeImage.write((int)b, (int)g, (int)r);
+			    } 
+				
+		}
+}
